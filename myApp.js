5948a5f32129b7d22e23ed1bee5a5aa2ee9d02e2
SumitRodrigues <sumitrod11@gmail.com>
2025-05-02 19:38:03 -0700

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,11 +1,19 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
 // console.log('Hello World!');
 
 app.get('/json', function(req, res) {
+
+    let message = "Hello json";
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+      } 
     res.json({
-        "message": "Hello json"
+        message
     })
   });
 
